e2aab08896116ec79dda48bf61ff84754eb1df0e
Pablo del Barrio <pablodbb@gmail.com>
2023-09-11 20:13:39 +0200

    added navbar

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 import Hero from "./Hero";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, Footer, Navbar };
