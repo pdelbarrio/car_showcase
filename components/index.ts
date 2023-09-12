afc8064e26aa7abacf53855fc46179ff8d4897af
Pablo del Barrio <pablodbb@gmail.com>
2023-09-12 19:57:29 +0200

    searchmanufacturer component finished

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Footer, Navbar };
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
